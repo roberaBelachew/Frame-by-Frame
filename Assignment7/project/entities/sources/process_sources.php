6d214f56490eb82d9b0c17e665c93c23b8365317
elenabls <elena21.blascu@gmail.com>
2025-11-04 21:55:40 +0100

    updated file

--- Assignment7/project/entities/sources/process_sources.php
+++ Assignment7/project/entities/sources/process_sources.php
@@ -1,29 +1,25 @@
 <?php
 include __DIR__ . '/../../auth_admin.php';
-
-// Enable error reporting for debugging
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
-// Include database connection
 require_once '../../db_connect.php';
-
-// Get the selected source type
 $type = $_POST['type'] ?? '';
 
-if ($type === '') {
+if ($type === '')
+{
     header("Location: feedback_sources.php?status=error&msg=Missing+Source+Type");
     exit;
 }
 
-try {
-    // Insert new source into the Sources table
+try
+{
     $stmt = $pdo->prepare("INSERT INTO Sources (type) VALUES (?)");
     $stmt->execute([$type]);
-
     header("Location: feedback_sources.php?status=success");
     exit;
-} catch (Exception $e) {
+} 
+catch (Exception $e) 
+{
     header("Location: feedback_sources.php?status=error&msg=" . urlencode($e->getMessage()));
     exit;
 }
