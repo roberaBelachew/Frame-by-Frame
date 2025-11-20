0613c051e296a9312efefb89f0b883ce8fcb5908
roberaBelachew <rbelachew@constructor.university>
2025-11-20 17:58:36 +0100

    Update test_connection.php

--- Assignment9/project/search/avgmovie/test_connection.php
+++ Assignment9/project/search/avgmovie/test_connection.php
@@ -1,5 +1,5 @@
 <?php
-// Enable error reporting
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -12,7 +12,7 @@ echo "Connection file loaded...<br>";
 if ($conn) {
     echo "Connection object exists!<br>";
     
-    // Test a simple query
+    
     $result = $conn->query("SELECT * FROM Movies LIMIT 5");
     
     if ($result) {
