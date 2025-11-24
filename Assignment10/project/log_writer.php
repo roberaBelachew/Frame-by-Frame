5d261ead229133120839bf93c3213937f5d999fe
alicia0804 <aliciastirbu104@gmail.com>
2025-11-24 20:55:22 +0100

    Update log_writer.php

--- Assignment10/project/log_writer.php
+++ Assignment10/project/log_writer.php
@@ -1,7 +1,4 @@
 <?php
-// project/log_writer.php
-// Append an Apache-style access line to project/logs/access.log
-// and optionally an error line to project/logs/error.log
 
 $log_dir = __DIR__ . '/logs';
 if (!is_dir($log_dir)) {
@@ -11,7 +8,7 @@ if (!is_dir($log_dir)) {
 $access_file = $log_dir . '/access.log';
 $error_file  = $log_dir . '/error.log';
 
-// Collect request info
+
 $ip      = $_SERVER['REMOTE_ADDR'] ?? '-';
 $time    = date('d/M/Y:H:i:s O'); // Apache common log time format
 $method  = $_SERVER['REQUEST_METHOD'] ?? 'GET';
@@ -22,14 +19,13 @@ $size    = '-';
 $ref     = $_SERVER['HTTP_REFERER'] ?? '-';
 $agent   = addslashes($_SERVER['HTTP_USER_AGENT'] ?? '-');
 
-// Build Apache common log format line
+
 $line = sprintf("%s - - [%s] \"%s %s %s\" %s %s \"%s\" \"%s\"\n",
     $ip, $time, $method, $uri, $protocol, $status, $size, $ref, $agent);
 
-// Write access log (append)
+
 file_put_contents($access_file, $line, FILE_APPEND | LOCK_EX);
 
-// Optionally, if a PHP error was set in the request context, log it.
-// You can call this file for errors like: file_put_contents($error_file, $errline, FILE_APPEND|LOCK_EX)
+
 ?>
 
